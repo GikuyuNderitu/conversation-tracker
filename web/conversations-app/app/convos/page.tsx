4afa582f66f403b39731a4b2ac14e6dceb19e233
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2022-11-14 02:02:54 -0800

    Connect client to fe server

--- web/conversations-app/app/convos/page.tsx
+++ web/conversations-app/app/convos/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <h1>Hello Conversation Page!</h1>
+class Note {
+  constructor(
+    public reply: string,
+    public id: string,
+    public content: string,
+  ) { }
+  static fromJson(): Note {
+    return new Note('a', 'b', 'c');
+  }
+
+}
+
+type NoteJson = { notes: Array<Note> }
+
+async function getNotes(): Promise<NoteJson> {
+  const res = await fetch('http://localhost:1337/notes');
+
+  return JSON.parse(await res.json()) as Promise<NoteJson>;
+
+}
+
+export default async function Page() {
+  const notes = (await getNotes())['notes'];
+  // console.log(res['notes'])
+  console.log(notes)
+  return <div>
+    <h1>Hello Conversation Page!</h1>
+    <ul>
+      {notes.map(note => <li key={note.id}>{note.content}</li>)}
+    </ul>
+  </div>
 }
