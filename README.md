90a629a2ee9cf50dd747b813b26e41cdbc898913
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2022-11-16 03:07:22 -0800

    Add documentation and License for pocket base. Add basic pocketbase startup to startup script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # conversation-tracker
-helps track a conversation
+Helps track a conversation
+
+## Pre-requirements
+Download [Pocketbase( v0.8.0-rc)](https://pocketbase.io/docs/)]
+Set an environment variable named "POCKET_BASE_ADDRESS".
--- start_backend.sh
+++ start_backend.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
-trap "exit" INT TERM ERR
+# trap "exit" INT TERM ERR
 trap "kill 0" EXIT
-export DB_PATH=file:///data/surrealdb/test
+export DB_PATH="file:///data/surrealdb/test"
+export POCKET_BASE_ADDRESS="127.0.0.1:1732"
 
 surreal start --log debug --user root --pass root -b 0.0.0.0:9021 &
+
+./pocketbase serve --http $POCKET_BASE_ADDRESS &
 
 go run ./service/notes &
 
