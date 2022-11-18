1e14e53e50af6bcd36aaae7edb4e2920b3ec102a
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2022-11-18 00:22:54 -0800

    Add basic layout to app

--- web/conversations-app/app/convos/layout.tsx
+++ web/conversations-app/app/convos/layout.tsx
@@ -3,9 +3,13 @@ export default function ConvoLayout({
 }: { children: React.ReactNode }) {
 
   return (
-    <div className="p-6">
-      <nav></nav>
-      <main>
+    <div>
+      <nav className="h-16 bg-on-primary-surface flex items-center">
+        <div className="mx-6">
+          <h1 className="text-4xl text-on-primary-surface-text">Conversations</h1>
+        </div>
+      </nav>
+      <main className="m-6">
 
         {children}
       </main>
