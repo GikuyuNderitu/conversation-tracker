ca46d7e319d2357042f4b3a8e72fb1cefbb5113d
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2022-12-24 13:28:30 -0800

    Add button to control deletion of note

--- web/conversations-app/app/convos/[id]/note_card.tsx
+++ web/conversations-app/app/convos/[id]/note_card.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Card from "../../../components/card";
 import ReplySection from "./reply_section";
+import { XCircleIcon } from '@heroicons/react/20/solid'
 
 type NoteCardProps = {
   content: string | undefined;
@@ -9,9 +11,31 @@ type NoteCardProps = {
 export default function NoteCard({ content, reply }: NoteCardProps) {
   return (
     <Card className="my-8">
+      <CloseButton onClick={() => console.log('closing')} />
       <p className="text-xl">{content}</p>
 
       <ReplySection reply={reply} />
     </Card>
+  )
+}
+
+type CloseButtonProps = {
+  onClick(event: React.MouseEvent<HTMLButtonElement>): void;
+}
+
+function CloseButton({ onClick }: CloseButtonProps) {
+  return (
+    <button className="
+    group 
+    float-right
+    mr-2 ml-auto
+      bg-stone-50 
+      rounded-[50%]"
+      onClick={onClick}>
+      <XCircleIcon className="
+      w-6 h-6
+      text-blue-500 stroke-blue-500 stroke-0 group-hover:stroke-1
+      transition-color duration-250" />
+    </button>
   )
 }
