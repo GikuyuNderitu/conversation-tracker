b638408bc53f203d2156e48fd4e2e70331f790c3
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2022-12-28 01:17:27 -0800

    Change the semantic order of the textarea for reply

--- web/conversations-app/app/convos/[id]/reply_section.tsx
+++ web/conversations-app/app/convos/[id]/reply_section.tsx
@@ -9,16 +9,17 @@ export default function ReplySection({ reply }: ReplySectionProps) {
   /// TODO(GikuyuNderitu): Mutate and update with new note data
   const [editing, setEditing] = useState(false)
   const editButtonText = editing ? 'Alter reply' : 'Edit reply'
+  const addButtonText = editing ? 'Cancel' : 'Add reply'
   const hasReply = reply !== undefined && reply !== '';
   return (
     <div className="flex flex-col items-start">
+      {hasReply ?
+        <button className="text-blue-500" onClick={() => setEditing(!editing)}>{editButtonText}</button> :
+        <button className="text-blue-500" onClick={() => setEditing(!editing)}>{addButtonText}</button>}
       {editing ?
         <textarea /> :
         <p className="text-base">{reply}</p>
       }
-      {hasReply ?
-        <button className="text-blue-500" onClick={() => setEditing(!editing)}>{editButtonText}</button> :
-        <button className="text-blue-500" onClick={() => setEditing(!editing)}>Add reply</button>}
     </div>
   )
 }
