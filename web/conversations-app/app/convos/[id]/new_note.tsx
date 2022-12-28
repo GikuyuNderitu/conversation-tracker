cae79ed57dcff4d812b9e28aac562f64d9aa5706
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2022-12-28 01:13:22 -0800

    Add ctrl+enter for new note & auto focus textarea

--- web/conversations-app/app/convos/[id]/new_note.tsx
+++ web/conversations-app/app/convos/[id]/new_note.tsx
@@ -3,7 +3,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { usePathname } from "next/navigation";
-import { useState } from "react"
+import React, { useRef, useState } from "react"
 import Card from "../../../components/card";
 import { Note } from '../../../models/notes';
 import { newNoteAtom } from "./new_note_state";
@@ -32,13 +32,15 @@ type NewNoteProps = {
   reload: VoidFunction;
 }
 
-
 export default function NewNote({ reload }: NewNoteProps) {
   const [enabled] = useAtom(newNoteAtom);
   const [noteContent, setNoteContent] = useState('');
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
   const { mutate } = useMutation({
     mutationFn: createNote,
     onSuccess() {
+      setNoteContent('')
+      textAreaRef.current?.focus()
       reload();
     },
   });
@@ -48,18 +50,24 @@ export default function NewNote({ reload }: NewNoteProps) {
   const conversationId = pathname?.split('/')[2] || '';
 
   const updateTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    console.log(e.target.value);
     setNoteContent(e.target.value);
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.ctrlKey && e.key === "Enter") {
+      console.log("Trying to submit something")
+      e.preventDefault()
+      submit();
+    }
   }
 
   const submit = () => {
     mutate({ content: noteContent, conversationId })
   }
 
-
   return (
     enabled ? <Card className="bg-white flex flex-col">
-      <textarea autoFocus className="form-textarea" value={noteContent} onChange={updateTextArea} />
+      <textarea ref={textAreaRef} onKeyDown={handleKeyDown} autoFocus className="form-textarea" value={noteContent} onChange={updateTextArea} />
       <button className="self-end mt-2 bg-accent px-3 py-1 rounded" onClick={submit}>Submit</button>
     </Card> : null
   )
